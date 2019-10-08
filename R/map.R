ec376982615c07677eddf8bdfcee109cbf380a78
Lennert verwimp <lennert.verwimp@student.ucll.be>
2019-10-08 14:09:35 +0200

    leaflet require fix

--- R/map.R
+++ R/map.R
@@ -1,18 +1,19 @@
 library(leaflet)
 
 mapMake <- function(latP = 50.87959, lngP = 4.70093){
+  require(leaflet)
   ma <<- leaflet() %>%
     addTiles() %>%  # Add default OpenStreetMap map tiles
     setView(zoom=4, lat = latP, lng = lngP)
-  
+
   con <- dbConnect(
     drv = RMariaDB::MariaDB(),
     db = 'Opencpu'
   )
-  
+
   res <- dbSendQuery(con, "SELECT SensorID, Latitude, Longitude from Sensor;")
   res2 <-dbFetch(res)
-  
+
   for (row in 1:nrow(res2))
   {
     sid <- res2[row, "SensorID"]
@@ -20,14 +21,15 @@ mapMake <- function(latP = 50.87959, lngP = 4.70093){
     longid <- res2[row, "Longitude"]
     mapAdd(latid, longid)
   }
-  
+
   dbClearResult(res)
   dbDisconnect(con)
-  
+
   mapGet()
 }
 
 mapAdd <- function(latP = 48.874065, lngP = 9.596336){
+  require(leaflet)
   ma <<- ma %>% addMarkers(lat = latP, lng = lngP)
   mapGet()
 }
